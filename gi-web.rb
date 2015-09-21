d2903ce2d7d185c1cec84c1800a4aa353a7c3b33
Anthony Lamorte <lamorte@gmail.com>
2015-09-20 22:22:24 -0400

    Sort issues list by the date that the requested label was applied

--- gi-web.rb
+++ gi-web.rb
@@ -6,17 +6,40 @@ require 'haml'
 GITHUB_USERNAME = ENV['GITHUB_USERNAME']
 GITHUB_TOKEN = ENV['GITHUB_TOKEN']
 QUERY = ENV['GI_QUERY']
+BASE_URL = 'https://api.github.com'
 
 get '/' do
-  api_response = open('https://api.github.com/search/issues?q=state:open type:pr user:thinkthroughmath label:"Needs QA"',
-       'User-Agent' => GITHUB_USERNAME,
-       'Authorization' => 'token ' + GITHUB_TOKEN,
-       'Content-Type' => 'application/json',
-       'Accept' => 'application/json').read
+  api_response = api_get("#{BASE_URL}/search/issues?q=#{QUERY}")
 
-  results = JSON.parse(api_response)
+  items = api_response['items']
 
-  @items = results['items']
+  label = QUERY.match(/label:\"(.*)\"/)[1]
 
-  haml :index
+  items = get_label_event_data(items, label)
+
+  items = sort_items(items)
+
+  haml :index, :locals => {items: items}
+end
+
+def get_label_event_data(items, label)
+  items.each do |item|
+    events = api_get("#{item['events_url']}?per_page=100")
+    label_event = events.select { |e| e['event'] == 'labeled' && e['label']['name'] == label }.last
+    item['label_event_data'] = label_event
+  end
+end
+
+def sort_items(items)
+  items.sort_by { |i| i['label_event_data']['created_at'] }
+end
+
+def api_get(url)
+  JSON.parse(
+    open(url,
+         'User-Agent' => GITHUB_USERNAME,
+         'Authorization' => 'token ' + GITHUB_TOKEN,
+         'Content-Type' => 'application/json',
+         'Accept' => 'application/json')
+    .read)
 end
